62d7f0669320537e3ef2ea47006ee7466d0ff378
Aran <aran@ermarian.net>
2013-03-08 15:48:49 +0100

    Only scan WA-members when aggregating endorsements.

--- endorsements.py
+++ endorsements.py
@@ -3,16 +3,19 @@ import argparse
 import progress
 
 def endorsements_region(region):
-    nations = ns.region_shard(region, 'nations').find('NATIONS').text.split(':')
     edges = set()
     incoming = {}
     outgoing = {}
-    for nation in progress.iterator(nations):
-        given = ns.nation_shard(nation, 'endorsements').find('ENDORSEMENTS').text.split(',')
+    for nation in progress.iterator(ns.wa_members(region)):
+        outgoing[nation] = set()
+        incoming[nation] = set()
+        given = ns.nation_shard(nation, 'endorsements').find('ENDORSEMENTS')
+        if not given.text:
+            continue
+        given = given.text.split(',')
         for giver in given:
             edges.add((giver,nation))
         incoming[nation] = set(given)
-        outgoing[nation] = set()
     for giver,receiver in edges:
         outgoing[giver].add(receiver)
     return incoming,outgoing,edges
